763c183b2b80e321382534e5600b2d679e06a43b
maxime <pheline.maxime@yahoo.fr>
2022-11-03 05:35:53 +0100

    Ajout des mails - phase 1

---
+++ src/Class/Mail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Class;
+
+use Mailjet\Client;
+use Mailjet\Resources;
+
+class Mail {
+
+    private $apiKey = "60d1bde9ef02072676045dacdaa61567" ;
+    private $apiKeySecret = "10e72f7734404a931235e0796f04cce9" ;
+
+    public function sendEmail($to_email, $to_name, $subject, $title, $content) {
+        $mj = new Client($this->apiKey, $this->apiKeySecret, true,['version' => 'v3.1']);
+        $body = [
+            'Messages' => [
+                [
+                    'From' => [
+                        'Email' => "lechatchouette@gmail.com",
+                        'Name' => "Un Jeu Pour Tous"
+                    ],
+                    'To' => [
+                        [
+                            'Email' => $to_email,
+                            'Name' => $to_name
+                        ]
+                    ],
+                    'TemplateID' => 4327553,
+                    'TemplateLanguage' => true,
+                    'Subject' => $subject,
+                    'Variables' => [
+                        'title' => $title,
+                        'content' => $content
+                    ]
+                ]
+            ]
+        ];
+        $response = $mj->post(Resources::$Email, ['body' => $body]);
+        $response->success() && dd($response->getData());
+    }
+
+}
---
+++ src/Controller/AccountOrderController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\OrderRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountOrderController extends AbstractController
+{
+    #[Route('/compte/mes-commandes', name: 'app_account_order')]
+    public function index(OrderRepository $orderRepository): Response
+    {
+        $orders = $orderRepository->findBy(
+            [
+                'isPaid' => true,
+                'user' => $this->getUser()
+            ],
+            ['createdAt' => 'DESC']) ;
+
+
+        return $this->render('account/order.html.twig',[
+            'orders' => $orders
+        ]);
+    }
+
+
+    #[Route('/compte/mes-commandes/{reference}', name: 'app_account_order_show')]
+    public function show(OrderRepository $orderRepository, $reference): Response
+    {
+        $order = $orderRepository->findOneBy(['reference' => $reference]);
+
+        if (!$order || $order->getUser() != $this->getUser()){
+            return $this->redirectToRoute('app_account_order');
+        }
+
+        return $this->render('account/order_show.html.twig',[
+            'order' => $order
+        ]);
+    }
+}
